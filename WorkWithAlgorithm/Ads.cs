2600290d82d559620ad1f817633f60ee50d801cd
Name <email@exaple.ru>
2023-08-10 09:05:09 +0300

    Final.

--- WorkWithAlgorithm/Ads.cs
+++ WorkWithAlgorithm/Ads.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WorkWithAlgorithm;
 
 namespace WorkWithAlgorithm
 {
-    internal class Ads
+    public class Ads
     {
+        public static void ShowAds()
+        {
+            Console.WriteLine("Посетите наш новый сайт с бесплатными играми free.games.for.a.fool.com");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Купите подписку на МыКомбо и слушайте музыку везде и всегда.");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Оформите премиум-подписку на наш сервис, чтобы не видеть рекламу.");
+            Thread.Sleep(3000);
+        }
     }
 }
--- WorkWithAlgorithm/Program.cs
+++ WorkWithAlgorithm/Program.cs
@@ -8,7 +8,19 @@ namespace WorkWithAlgorithm
         static void Main(string[] args)
         {
            Users[] users = Users.UsersInfo();
-            
+            foreach (var user in users) 
+            {
+                Console.WriteLine($"{user.Name},Здравствуйте\n");
+                if(user.IsPremium == false)
+                {
+                    Ads.ShowAds();
+                }
+                else
+                {
+                    Console.WriteLine("У вас премиум-подписка!");
+                }
+            }
+            Console.WriteLine("Конец.");
         }
     }
 }
