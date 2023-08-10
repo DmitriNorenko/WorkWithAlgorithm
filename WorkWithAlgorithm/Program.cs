5372820ba7e06a931d59ea56db917348d0902f2a
Name <email@exaple.ru>
2023-08-10 08:38:18 +0300

    Добавьте файлы проекта.

---
+++ WorkWithAlgorithm/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace WorkWithAlgorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
